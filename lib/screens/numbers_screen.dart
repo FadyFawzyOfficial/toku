8af549660064d7eb41ce2d46782e77cea1682a6a
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-09-23 22:03:28 +0300

    5. Make the WordListTile Widget use the Word Object

--- lib/screens/numbers_screen.dart
+++ lib/screens/numbers_screen.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 
+import '../models/word.dart';
 import '../widgets/word_list_tile.dart';
 
 class NumbersScreen extends StatelessWidget {
   const NumbersScreen({super.key});
+
+  static const one = Word(
+    image: 'assets/images/numbers/number_one.png',
+    japanese: 'one',
+    english: 'one',
+    sound: '',
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +21,7 @@ class NumbersScreen extends StatelessWidget {
       ),
       body: ListView(
         children: const [
-          WordListTile(),
+          WordListTile(word: one),
         ],
       ),
     );
--- lib/widgets/word_list_tile.dart
+++ lib/widgets/word_list_tile.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../models/word.dart';
+
 class WordListTile extends StatelessWidget {
-  const WordListTile({super.key});
+  final Word word;
+  const WordListTile({super.key, required this.word});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      contentPadding: const EdgeInsetsDirectional.all(0),
       tileColor: Colors.amber,
       iconColor: Colors.white,
-      leading: Image.asset('assets/images/numbers/number_one.png'),
-      title: const Text('One'),
-      subtitle: const Text('One'),
+      contentPadding: const EdgeInsetsDirectional.all(0),
+      leading: word.image == null ? null : Image.asset(word.image!),
+      title: Text(word.japanese),
+      subtitle: Text(word.english),
       trailing: IconButton(
         onPressed: () {},
         icon: const Icon(Icons.play_arrow),
