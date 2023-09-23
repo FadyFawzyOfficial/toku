9148c46a6fa7e56148e6a93af1611ab813015a1d
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-09-23 21:58:11 +0300

    4. Build Model to Represent each Word

---
+++ lib/models/word.dart
@@ -0,0 +1,13 @@
+class Word {
+  final String? image;
+  final String japanese;
+  final String english;
+  final String sound;
+
+  const Word({
+    this.image,
+    required this.japanese,
+    required this.english,
+    required this.sound,
+  });
+}
