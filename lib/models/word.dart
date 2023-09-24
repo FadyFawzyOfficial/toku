a6d15043de974a57716753d0a63d5191c19e41ac
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-09-24 19:56:46 +0300

    6. Use ListView.builder() Widget to build List of Word Objects

--- lib/models/word.dart
+++ lib/models/word.dart
@@ -11,3 +11,66 @@ class Word {
     required this.sound,
   });
 }
+
+const List<Word> numbers = [
+  Word(
+    english: 'one',
+    japanese: 'ichi',
+    sound: 'assets/sounds/numbers/number_one_sound.mp3',
+    image: 'assets/images/numbers/number_one.png',
+  ),
+  Word(
+    english: 'two',
+    japanese: 'Ni',
+    sound: 'assets/sounds/numbers/number_two_sound.mp3',
+    image: 'assets/images/numbers/number_two.png',
+  ),
+  Word(
+    english: 'three',
+    japanese: 'San',
+    sound: 'assets/sounds/numbers/number_one_sound.mp3',
+    image: 'assets/images/numbers/number_three.png',
+  ),
+  Word(
+    english: 'four',
+    japanese: 'Shi',
+    sound: 'assets/sounds/numbers/number_one_sound.mp3',
+    image: 'assets/images/numbers/number_four.png',
+  ),
+  Word(
+    english: 'five',
+    japanese: 'Go',
+    sound: 'assets/sounds/numbers/number_one_sound.mp3',
+    image: 'assets/images/numbers/number_five.png',
+  ),
+  Word(
+    english: 'six',
+    japanese: 'Roku',
+    sound: 'assets/sounds/numbers/number_one_sound.mp3',
+    image: 'assets/images/numbers/number_six.png',
+  ),
+  Word(
+    english: 'seven',
+    japanese: 'Sebun',
+    sound: 'assets/sounds/numbers/number_one_sound.mp3',
+    image: 'assets/images/numbers/number_seven.png',
+  ),
+  Word(
+    english: 'eight',
+    japanese: 'hachi',
+    sound: 'assets/sounds/numbers/number_one_sound.mp3',
+    image: 'assets/images/numbers/number_eight.png',
+  ),
+  Word(
+    english: 'nine',
+    japanese: 'Kyū',
+    sound: 'assets/sounds/numbers/number_one_sound.mp3',
+    image: 'assets/images/numbers/number_nine.png',
+  ),
+  Word(
+    english: 'ten',
+    japanese: 'Jū',
+    sound: 'assets/sounds/numbers/number_one_sound.mp3',
+    image: 'assets/images/numbers/number_ten.png',
+  ),
+];
--- lib/screens/numbers_screen.dart
+++ lib/screens/numbers_screen.dart
@@ -6,23 +6,17 @@ import '../widgets/word_list_tile.dart';
 class NumbersScreen extends StatelessWidget {
   const NumbersScreen({super.key});
 
-  static const one = Word(
-    image: 'assets/images/numbers/number_one.png',
-    japanese: 'one',
-    english: 'one',
-    sound: '',
-  );
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Numbers'),
       ),
-      body: ListView(
-        children: const [
-          WordListTile(word: one),
-        ],
+      body: ListView.builder(
+        padding: EdgeInsets.zero,
+        physics: const ClampingScrollPhysics(),
+        itemCount: numbers.length,
+        itemBuilder: (context, index) => WordListTile(word: numbers[index]),
       ),
     );
   }
